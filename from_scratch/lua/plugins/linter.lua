8dd67bcafcc4219ea0e74b7f46bf047d52bae3c1
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2023-01-27 11:54:43 -0500

    Add dockerfile, change activation for ansible-lint

--- from_scratch/lua/plugins/linter.lua
+++ from_scratch/lua/plugins/linter.lua
@@ -4,12 +4,12 @@ return {
     config = function()
       require('lint').linters_by_ft = {
         ruby   = {'ruby'},
-        yaml   = {'ansible_lint', 'yamllint'},
-        python = {'pylint'}
+        yaml   = {'yamllint'},
+        ansible = {'ansible_lint'}, -- set ft=yaml.ansible
+        dockerfile = {'hadolint'}
       }
     end,
     init = function()
-
       vim.api.nvim_create_autocmd({ "BufWritePost" }, {
         callback = function()
         require("lint").try_lint()
