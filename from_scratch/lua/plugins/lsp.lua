ea7aa833c94fe89dfce19cb5d3ddb786870f434c
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2023-01-27 11:55:06 -0500

    Use lsp-zero instead.

--- from_scratch/lua/plugins/lsp.lua
+++ from_scratch/lua/plugins/lsp.lua
@@ -1,64 +1,53 @@
 return {
   {
-    'neovim/nvim-lspconfig'
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    dependencies = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},         -- Required
+      {'hrsh7th/cmp-nvim-lsp'},     -- Required
+      {'hrsh7th/cmp-buffer'},       -- Optional
+      {'hrsh7th/cmp-path'},         -- Optional
+      {'saadparwaiz1/cmp_luasnip'}, -- Optional
+      {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},             -- Required
+      {'rafamadriz/friendly-snippets'}, -- Optional
+    },
+    config = function()
+      -- Learn the keybindings, see :help lsp-zero-keybindings
+      -- Learn to configure LSP servers, see :help lsp-zero-api-showcase
+      local lsp = require('lsp-zero')
+      lsp.preset('recommended')
+
+      -- See :help lsp-zero-preferences
+      lsp.set_preferences({
+        set_lsp_keymaps = true, -- set to false if you want to configure your own keybindings
+        manage_nvim_cmp = true, -- set to false if you want to configure nvim-cmp on your own
+
+        -- Opt-out of mason.nvim
+        suggest_lsp_servers = false,
+        setup_servers_on_start = false,
+        call_servers = 'global'
+      })
+      local lsp_opts = {
+        single_file_support = false
+      }
+      lsp.setup_servers({'bashls', 'ruff_lsp', 'rust_analyzer',
+        -- Web Development
+        'quick_lint_js', 'tsserver',
+        -- DevOps
+        'ansiblels', 'terraformls',
+        opts = lsp_opts})
+
+      -- (Optional) Configure lua language server for neovim
+      lsp.nvim_workspace()
+
+      lsp.setup()
+    end
   }
 }
-
-
--- -- [[ nvim-lspconfig ]]
--- if prequire('lspconfig') then
---   -- Mappings.
---   -- See `:help vim.diagnostic.*` for documentation on any of the below functions
---   local opts = { noremap=true, silent=true }
---   vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
---   vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
---   vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
---   vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
---
---   -- Use an on_attach function to only map the following keys
---   -- after the language server attaches to the current buffer
---   local on_attach = function(client, bufnr)
---     -- Enable completion triggered by <c-x><c-o>
---     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
---
---     -- Mappings.
---     -- See `:help vim.lsp.*` for documentation on any of the below functions
---     local bufopts = { noremap=true, silent=true, buffer=bufnr }
---     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
---     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
---     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
---     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
---     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
---     vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
---     vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
---     vim.keymap.set('n', '<space>wl', function()
---       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
---     end, bufopts)
---     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
---     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
---     vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
---     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
---     vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
---   end
---
---   local lsp_flags = {
---     -- This is the default in Nvim 0.7+
---     debounce_text_changes = 150,
---   }
---   require('lspconfig')['pyright'].setup{
---       on_attach = on_attach,
---       flags = lsp_flags,
---   }
---   require('lspconfig')['tsserver'].setup{
---       on_attach = on_attach,
---       flags = lsp_flags,
---   }
---   require('lspconfig')['rust_analyzer'].setup{
---       on_attach = on_attach,
---       flags = lsp_flags,
---       -- Server-specific settings...
---       settings = {
---         ["rust-analyzer"] = {}
---       }
---   }
--- end
