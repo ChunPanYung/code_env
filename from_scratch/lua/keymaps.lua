afc33dc8dc7d526b6bf1304e9599818fdb8beb53
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2023-02-04 23:45:37 -0500

    Change to use <leader> key for insert single char.

--- from_scratch/lua/keymaps.lua
+++ from_scratch/lua/keymaps.lua
@@ -12,20 +12,11 @@ keymap("i", "<C-s>", "<Esc>:write<CR>i", { silent = true }) -- map <M-BS> to del
 
 -- [[ Normal mode ]]
 keymap("n", "<C-_>", "gcc", { silent = true }) -- Map <C-_> and <C-/> to gcc
--- keymap("n", "<Leader>i", "i_<Esc>r", opts) -- insert single character before cursor
--- keymap("n", "<Leader>a", "a_<Esc>r", opts) -- insert single character after cursor
 -- [[ insert single character before cursor ]]
 vim.cmd([[
-nnoremap ,i :exec "normal i".nr2char(getchar())."\e"<CR>
-nnoremap ,a :exec "normal a".nr2char(getchar())."\e"<CR>
+nnoremap <Leader>i :exec "normal i".nr2char(getchar())."\e"<CR>
+nnoremap <Leader>a :exec "normal a".nr2char(getchar())."\e"<CR>
 ]])
-
--- [[ buffer keymaps ]]
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
-
-keymap("n", "<Leader>Z", ":write<CR>:bdelete<CR>", opts)
-keymap("n", "<Leader>q", ":bprevious<CR>:bdelete #<CR><C-l>", {silent = true})
 
 -- move up/down by visual line instead of text line
 keymap("n", "j", "gj", opts)
